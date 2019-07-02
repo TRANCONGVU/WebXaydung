2909bbd8548c307ed68e6cb434ca57939bb68ce4
TRANCONGVU <vndvip10>
2019-07-02 17:16:44 +0700

    trangchu

--- resources/views/master-layout.blade.php
+++ resources/views/master-layout.blade.php
@@ -20,18 +20,18 @@
 	<link rel="stylesheet" type="text/css" href="css/header.css">
 	<link rel="stylesheet" type="text/css" href="css/responsive.css">
 	<link rel="stylesheet" href="css/gioithieu.css">
-	
-	
+
+
 	<script src="lib\OwlCarousel2-2.3.4\owl.carousel.min.js"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
 	<script type="text/javascript" src="lib/bootstrap_4.0.0/js/bootstrap.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" ></script>
 	<script type="text/javascript" src="lib/OwlCarousel2-2.3.4/owl.carousel.js"></script>
-	
+
 </head>
 <body>
-	
+
 @include('header')
 @yield('content')
 @include('footer')
