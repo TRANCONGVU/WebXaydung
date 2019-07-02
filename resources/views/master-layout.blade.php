d797696e0f02122163eeef4569bd34e0492030b4
thor-bui <thob2105@gmail.com>
2019-07-02 17:23:11 +0700

    cac trang con

--- resources/views/master-layout.blade.php
+++ resources/views/master-layout.blade.php
@@ -19,10 +19,12 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/header.css">
 	<link rel="stylesheet" type="text/css" href="css/responsive.css">
-	<link rel="stylesheet" href="css/gioithieu.css">
+	<link rel="stylesheet" type="text/css" href="css/gioithieu.css">
+	<link rel="stylesheet" type="text/css" href="css/hosothietke.css">
+
 	
 	
-	<script src="lib\OwlCarousel2-2.3.4\owl.carousel.min.js"></script>
+	
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
 	<script type="text/javascript" src="lib/bootstrap_4.0.0/js/bootstrap.min.js"></script>
---
+++ resources/views/pages/noithat.blade.php
@@ -0,0 +1,229 @@
+@extends('master-layout')
+@section('content')
+
+<!-- noi that -->
+<main>
+    <!-- breadcrumb -->
+    <section class="page-header">
+        <div class="container">
+            <div class="breadcrumb-wrapp">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item active" aria-current="page">Trang chu</li>
+                    <li class="breadcrumb-item">
+                        Thiết kế thi công nội thất</li>
+                </ul>
+            </div>
+            <div>
+                <h1 class="page-tittle">Thiết kế thi công nội thất</h1>
+            </div>
+        </div>
+
+    </section>
+
+    <hr>
+    <!--option  -->
+    <section class="option text-center">
+        <div class="container">
+            <div class="option-t">
+                <select name="" id="">
+                    <option value="">công trình tại các tỉnh/thành</option>
+                    <option value="">hà nội</option>
+                    <option value="">hồ chí minh</option>
+                    <option value="">đà nẵng</option>
+                    <option value="">thái bình</option>
+                    <option value="">cần thơ</option>
+                    <option value="">nghệ an</option>
+
+                </select>
+
+                <div class="search-box">
+                    <input type="text" placeholder="tìm theo mã số công trình">
+                    <button type="button" class="btn btn-secondary"> <i class="fas fa-search"></i></button>
+                </div>
+            </div>
+
+
+
+            <div class="text-option">
+                <a href="#">Lorem ipsum dolor sit - amet consectetur adipisicinf san aditi</a>
+
+            </div>
+        </div>
+
+    </section>
+    <hr>
+
+    <!-- banner -->
+    <section class="banner-nt">
+        <div class="container">
+            <div class="img-banner d-flex">
+                <a href="#">
+                    <img src="https://shac.vn/wp-content/uploads/2018/10/Ads-Tra-cuu-thuoc-lo-ban.jpg" alt="">
+                </a>
+                <a href="#">
+                    <img src="https://shac.vn/wp-content/uploads/2018/05/Thiet-ke-thi-cong-noi-that-khach-san-3.jpg"
+                        alt="">
+                </a>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- gallery -->
+    <section class="demo-thiet-ke">
+        <div class="container">
+            <h2>Hình ảnh 100+ thi công nội thất chung cư mà CĐT rất hài lòng</h2>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="hover01 colum ">
+                        <div>
+                            <figure><a href="#"><img src="https://picsum.photos/300/200?image=244" /></a></figure>
+                            <p class="text-demo"><a href="#">Hạng III
+                                [Hỏi – Đáp] #1820: Công trình cấp IV, sửa chữa có cần chứng chỉ năng lực hoạt động xây
+                                dựng</a></p>
+                        </div>
+                    </div>
+                </div>
+
+             
+
+            </div>
+        </div>
+
+        <div class="pagin">
+            <nav aria-label="Page navigation example ">
+                <ul class="pagination justify-content-center">
+                    <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item "><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        </div>
+    </section>
+
+
+
+
+
+
+    @include('content-chung')
+</main>
+
+
+
+@endsection
