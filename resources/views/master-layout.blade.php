2b3f9ebfcd5974486d8d86221664d333efd5a13c
TRANCONGVU <vndvip10>
2019-07-04 14:10:47 +0700

    hihi

--- resources/views/master-layout.blade.php
+++ resources/views/master-layout.blade.php
@@ -19,18 +19,12 @@
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/header.css">
 	<link rel="stylesheet" type="text/css" href="css/responsive.css">
-<<<<<<< HEAD
 	<link rel="stylesheet" type="text/css" href="css/gioithieu.css">
 	<link rel="stylesheet" type="text/css" href="css/hosothietke.css">
 
-	
-	
-	
-=======
-	
 
-	<script src="lib\OwlCarousel2-2.3.4\owl.carousel.min.js"></script>
->>>>>>> 034cd5279580884bcf6f3e51749f8651f8bbde62
+
+
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
 	<script type="text/javascript" src="lib/bootstrap_4.0.0/js/bootstrap.min.js"></script>
